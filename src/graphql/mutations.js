fb7fbcf635d68360825b36f933f402f8192745c5
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-06-02 12:02:45 +0300

    ex 10.15 corrected useRepositories to update the fetch and added missing .edges to RepositoryList (4)

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,13 +1,3 @@
-// import { gql } from "apollo-boost";
-
-// export const SIGN_IN = gql`
-//   mutation authorize($credentials: AuthorizeInput!) {
-//       authorize(credentials: $credentials) {
-//         accessToken
-//       }
-//     }
-// `;
-
 import { gql } from "apollo-boost";
 
 export const SIGN_IN = gql`
